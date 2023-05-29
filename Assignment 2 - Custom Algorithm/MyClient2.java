bb46f5380a93db974cc068e4b72a904009014a9f
Hashy01 <hasham.masood02@gmail.com>
2023-05-29 23:05:43 +1000

    Refactored Code + Code Style + Report

--- Assignment 2 - Custom Algorithm/MyClient2.java
+++ Assignment 2 - Custom Algorithm/MyClient2.java
@@ -3,133 +3,115 @@ import java.net.*;
 
 public class MyClient2 {
 
-  static Socket s;
-  static DataOutputStream dout;
-  static BufferedReader dis;
+    static Socket hostSocket;
+    static DataOutputStream dout;
+    static BufferedReader dis;
 
-  static void clientSend(String message) throws IOException {
-    dout.write((message + "\n").getBytes());
-    dout.flush();
-    System.out.println("Client Sent: " + message);
-  }
-  static String clientRead() throws IOException {
-    String message = (String) dis.readLine();
-    System.out.println("Server Message: " + message);
-    return message;
-  }
-
-  public static void main(String args[]) {
-
-    try {
-      s = new Socket("localhost", 50000);
-      dis = new BufferedReader(new InputStreamReader(s.getInputStream()));
-      dout = new DataOutputStream(s.getOutputStream());
-
-      String serverResponse = "";
-
-      // Send "HELO"
-      clientSend("HELO");
-      serverResponse = clientRead();
-      System.out.println("RCVD " + serverResponse);
-
-      // Send "AUTH"
-      String username = System.getProperty("user.name");
-      clientSend("AUTH " + username);
-      serverResponse = clientRead();
-      System.out.println("RCVD " + serverResponse);
-
-      // Send "REDY"
-      clientSend("REDY"); // Initial REDY before the loop
-      serverResponse = clientRead();
-      System.out.println("RCVD " + serverResponse);
-
-      while (!serverResponse.contains("NONE")) {
-        if (serverResponse.contains("JCPL") || serverResponse.contains("JOBN")) {
-          if (serverResponse.contains("JOBN")) {
-            String[] jobData = serverResponse.split(" ");
-            int jobCore = Integer.valueOf(jobData[4]);
-            int jobMemory = Integer.valueOf(jobData[5]);
-            int jobDisk = Integer.valueOf(jobData[6]);
-
-            clientSend("GETS Avail " + jobCore + " " + jobMemory + " " + jobDisk);
-            String test = clientRead();
-            // dout.write(("GETS Capable " + jobCore + " " + jobMemory + " " + jobDisk + "\n").getBytes());
-            System.out.println("SENT GETS Capable " + jobCore + " " + jobMemory + " " + jobDisk);
-
-            String[] serverInfo = test.split(" ");
-            int nrecs = Integer.valueOf(serverInfo[1]);
-
-            if (nrecs == 0){
-              clientSend("OK");
-              clientRead();
-              clientSend("GETS Capable " + jobCore + " " + jobMemory + " " + jobDisk);
-              test = clientRead();
-              serverInfo = test.split(" ");
-              nrecs = Integer.valueOf(serverInfo[1]);
-            }
-
-
-
-            // SEND "OK"
-            clientSend("OK");
-            System.out.println("SENT OK");
-
-            String serverData = "";
-            String firstServer = "";
-
-            boolean serverLocatedFlag = false;
-
-            for (int i = 0; i < nrecs; i++) {
-              String currentServerData = dis.readLine();
-              String[] serverDataArr = currentServerData.split(" ");
-              int serverCores = Integer.parseInt(serverDataArr[4]);
-              if (i == 0) {
-                firstServer = currentServerData;
-              }
-              if (serverCores >= jobCore && !serverLocatedFlag) {
-                serverData = currentServerData;
-                serverLocatedFlag = true;
-              }
-            }
-
-            if (!serverLocatedFlag) {
-              serverData = firstServer;
-            }
-
-            clientSend("OK");
-            System.out.println("SENT OK");
-            clientRead();
-
-            String[] serverDataArr = serverData.split(" ");
-            String serverType = serverDataArr[0];
-            String serverID = serverDataArr[1];
-
-            dout.write(("SCHD " + jobData[2] + " " + serverType + " " + serverID + "\n").getBytes());
-            System.out.println("SENT SCHD " + jobData[2] + " " + serverType + " " + serverID);
-            serverResponse = clientRead();
-          }
-
-          clientSend("REDY");
-          System.out.println("SENT REDY");
-        } else {
-          System.out.println("message: " + serverResponse);
-        }
-
-        serverResponse = clientRead();
-        if (serverResponse != null) {
-          System.out.println("RCVD " + serverResponse);
-        } else {
-          System.out.println("RCVD null message");
-        }
-      }
-
-      clientSend("QUIT");
-      serverResponse = clientRead();
-      System.out.println("RCVD " + serverResponse);
-      dout.close();
-      s.close();
-    } catch (Exception e) {
-      System.out.println(e);
+    static void clientSend(String message) throws IOException {
+        dout.write((message + "\n").getBytes());
+        dout.flush();
+        System.out.println("Client Sent: " + message);
     }
-  }
+    static String clientRead() throws IOException {
+        String message = (String) dis.readLine();
+        System.out.println("Server Message: " + message);
+        return message;
+    }
+
+    public static void main(String args[]) {
+
+        try {
+            hostSocket = new Socket("localhost", 50000);
+            dis = new BufferedReader(new InputStreamReader(hostSocket.getInputStream()));
+            dout = new DataOutputStream(hostSocket.getOutputStream());
+
+            String serverResponse = "";
+
+            // Send "HELO"
+            clientSend("HELO");
+            serverResponse = clientRead();
+
+            // Send "AUTH"
+            String username = System.getProperty("user.name");
+            clientSend("AUTH " + username);
+            serverResponse = clientRead();
+
+            // Send "REDY"
+            clientSend("REDY"); // Initial REDY before the loop
+            serverResponse = clientRead();
+
+            while (!serverResponse.contains("NONE")) {
+                if (serverResponse.contains("JCPL") || serverResponse.contains("JOBN")) {
+                    if (serverResponse.contains("JOBN")) {
+                        String[] jobData = serverResponse.split(" ");
+                        int jobCore = Integer.valueOf(jobData[4]);
+                        int jobMemory = Integer.valueOf(jobData[5]);
+                        int jobDisk = Integer.valueOf(jobData[6]);
+
+                        clientSend("GETS Avail " + jobCore + " " + jobMemory + " " + jobDisk);
+                        String getsResponse = clientRead();
+                        String[] serverInfo = getsResponse.split(" ");
+                        int nrecs = Integer.valueOf(serverInfo[1]);
+
+                        if (nrecs == 0) {
+                            clientSend("OK");
+                            clientRead();
+                            clientSend("GETS Capable " + jobCore + " " + jobMemory + " " + jobDisk);
+                            getsResponse = clientRead();
+                            serverInfo = getsResponse.split(" ");
+                            nrecs = Integer.valueOf(serverInfo[1]);
+                        }
+
+                        // SEND "OK"
+                        clientSend("OK");
+
+                        String serverData = "";
+                        String firstServer = "";
+
+                        boolean serverLocatedFlag = false;
+
+                        for (int i = 0; i < nrecs; i++) {
+                            String currentServerData = dis.readLine();
+                            String[] serverDataArr = currentServerData.split(" ");
+                            int serverCores = Integer.parseInt(serverDataArr[4]);
+                            if (i == 0) {
+                                firstServer = currentServerData;
+                            }
+                            if (serverCores >= jobCore && !serverLocatedFlag) {
+                                serverData = currentServerData;
+                                serverLocatedFlag = true;
+                            }
+                        }
+
+                        if (!serverLocatedFlag) {
+                            serverData = firstServer;
+                        }
+
+                        clientSend("OK");
+                        clientRead();
+
+                        String[] serverDataArr = serverData.split(" ");
+                        String serverType = serverDataArr[0];
+                        String serverID = serverDataArr[1];
+
+                        dout.write(("SCHD " + jobData[2] + " " + serverType + " " + serverID + "\n").getBytes());
+                        serverResponse = clientRead();
+                    }
+
+                    clientSend("REDY");
+
+                }
+
+                serverResponse = clientRead();
+
+            }
+
+            clientSend("QUIT");
+            serverResponse = clientRead();
+            dout.close();
+            hostSocket.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
 }
